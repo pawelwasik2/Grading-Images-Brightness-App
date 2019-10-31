e6ccbe470b78c72b31620e9e91ef3c8f5a1bf37a
pawelwasik2 <pawelwasik18@gmail.com>
2019-10-31 22:41:35 +0100

    little tests

--- src/main/scala/com/Main.scala
+++ src/main/scala/com/Main.scala
@@ -1,6 +1,40 @@
 package com
 
+import java.io.File
+import java.io.IOException
+import java.awt.image.BufferedImage
+import javax.imageio.ImageIO
+
+
 object Main extends App {
-  val str = "Hello World..."
-  println(str)
+  //val file = new File("./bright/a.jpg")
+
+
+  //val resourcePath = getClass.getResource("/bright/a.jpg")
+
+  //println(resourcePath.getPath)
+
+  val file = new File("C:\\Users\\Pawel\\Desktop\\IntellijProjects\\ScalacIntreview\\src\\main\\resources\\a.jpg")
+  val image: BufferedImage = ImageIO.read(file)
+  val width = image.getWidth
+  val height = image.getHeight
+  println(width, height)
+  val singlePixel = image.getRGB(0, 0)
+
+  val blue = singlePixel & 0xff
+  val red = (singlePixel >> 16) & 0xff
+  val green = (singlePixel >> 8) & 0xff
+  val alpha = (singlePixel >> 24) & 0xff
+
+
+
+
+  println(alpha, red, green, blue)
+
+
+  for(i <- 0 to height){
+    for(j <- 0 to width){
+
+    }
+  }
 }
