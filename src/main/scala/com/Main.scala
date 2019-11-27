12010325848c44195dc574ef5b8e7dc280d78738
pawelwasik2 <pawelwasik18@gmail.com>
2019-11-27 01:12:10 +0100

    in progress

--- src/main/scala/com/Main.scala
+++ src/main/scala/com/Main.scala
@@ -28,10 +28,48 @@ object Main extends App {
   val folder = new File(pathSrcDir)
   val photos = folder.listFiles.filter(_.isFile).toList
 
+  photos.map(file =>
+    ImageIO.read(file)
+      .getRGB(0, 0, getWidth)
+    //print(file)
+    //val img = ImageIO.read(file)
+  )
+    /*val width = image.getWidth
+    val height = image.getHeight
+
+    var V = 0.0
+
+    for(i <- 0 until width ){
+      for(j <- 0 until height){
+        val singlePixel = image.getRGB(i, j)
+        V = V + Calc.getMaxOfColors(singlePixel)
+      }
+    }
+
+    //set score based on V from HSV color scale and make new name depends on it
+    val vAVG = V/(width*height)
+    val score = 100 - (vAVG * 100).round
+    val newName = file.getName.split("\\.")
+    if(score > border){
+      newName(0) = newName(0) + "_dark_" + score
+    }else{
+      newName(0) = newName(0) + "_bright_" + score
+    }
+
+    //copy file to "out" dir with new name
+    val source = file
+    val destination = ".\\" + resultDir + "\\" + newName.mkString(".")
+    val inChannel = new FileInputStream(source).getChannel
+    val outChannel = new FileOutputStream(destination).getChannel
+    outChannel.transferFrom(inChannel, 0, inChannel.size())
+    inChannel.close()
+    outChannel.close()
+  )*/
+
 
 
   //iterate through every single pixel from photo and calculate average V for all of them
-  for(file <- photos){
+  /*for(file <- photos){
     val image: BufferedImage = ImageIO.read(file)
     val width = image.getWidth
     val height = image.getHeight
@@ -63,5 +101,5 @@ object Main extends App {
     outChannel.transferFrom(inChannel, 0, inChannel.size())
     inChannel.close()
     outChannel.close()
-  }
+  }*/
 }
