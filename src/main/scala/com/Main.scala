a18fedfc91257020cdee0a2c6ac3366f9066967c
pawelwasik2 <pawelwasik18@gmail.com>
2019-11-27 19:45:12 +0100

    did changes from review, I think it's ok, I'll check it in like 2 month

--- src/main/scala/com/Main.scala
+++ src/main/scala/com/Main.scala
@@ -6,6 +6,11 @@ import java.awt.image.BufferedImage
 import javax.imageio.ImageIO
 import java.io.File.separator
 import com.typesafe.config._
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.Paths
+import java.nio.file.StandardCopyOption._
+
 
 object Main extends App {
   //load in, out dir and cutoff point from the conf file
@@ -15,33 +20,36 @@ object Main extends App {
   val border = conf.getInt("vars.point")
 
   //get path of every single jpg and png from in directory
-  //val folder = new File(srcDir)
   val photos = new File(srcDir).listFiles.filter(_.isFile).toList
 
-  print(photos)
-  /*photos.map{el => {
+  //todo: for in FP??
+  //todo: 0 in return??
+  //todo: try to remove all vars
+  photos.map{el => {
     val image = ImageIO.read(el)
-    for(i <- 0 until image.getWidth ){
-      for(j <- 0 until image.getHeight){
-        val singlePixel = image.getRGB(i, j)
-        V = V + Calc.getMaxOfColors(singlePixel)
-      }
-      }
-    0
-    }}
-    val width = image.getWidth
-    val height = image.getHeight
-
-    var V = 0.0
-
-    for(i <- 0 until width ){
-      for(j <- 0 until height){
+    var V: Double = 0
+    for(i <- 0 until image.getWidth ) {
+      for (j <- 0 until image.getHeight) {
         val singlePixel = image.getRGB(i, j)
         V = V + Calc.getMaxOfColors(singlePixel)
       }
     }
+    val vAVG = V/(image.getWidth*image.getHeight)
+    val score = 100 - (vAVG * 100).round
+    val newName = el.getName.split("\\.")
+    if(score > border){
+      newName(0) = newName(0) + "_dark_" + score
+    }else{
+      newName(0) = newName(0) + "_bright_" + score
+    }
 
-    //set score based on V from HSV color scale and make new name depends on it
+    val destination = resultDir + separator + newName.mkString(".")
+    val src: Path = Paths.get(el.getPath)
+    val dest: Path = Paths.get(destination)
+    Files.copy(src, dest, REPLACE_EXISTING)
+  }}
+  //Files.copy(el.getPath, destination)
+    /*//set score based on V from HSV color scale and make new name depends on it
     val vAVG = V/(width*height)
     val score = 100 - (vAVG * 100).round
     val newName = file.getName.split("\\.")
@@ -59,7 +67,7 @@ object Main extends App {
     outChannel.transferFrom(inChannel, 0, inChannel.size())
     inChannel.close()
     outChannel.close()
-  )*/
+  )
 
 
 
@@ -96,5 +104,5 @@ object Main extends App {
     outChannel.transferFrom(inChannel, 0, inChannel.size())
     inChannel.close()
     outChannel.close()
-  }*/
+  }*/*/
 }
