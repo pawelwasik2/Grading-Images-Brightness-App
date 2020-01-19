cffc7a989965df269153744998b922d5786baa66
pawelwasik2 <pawelwasik18@gmail.com>
2020-01-19 16:06:36 +0100

    some changes but its not what i need tbh

--- build.sbt
+++ build.sbt
@@ -5,3 +5,5 @@ version := "0.1"
 scalaVersion := "2.13.1"
 
 libraryDependencies += "com.typesafe" % "config" % "1.4.0"
+
+libraryDependencies += "org.apache.commons" % "commons-parent" % "48"
--- src/main/scala/com/Main.scala
+++ src/main/scala/com/Main.scala
@@ -1,43 +1,52 @@
 package com
 
 import java.io._
-import java.awt.image.BufferedImage
 import javax.imageio.ImageIO
 import java.io.File.separator
 import com.typesafe.config._
 import java.nio.file.Files
-import java.nio.file.Path
 import java.nio.file.Paths
 import java.nio.file.StandardCopyOption._
+//import org.apache.commons.io.FilenameUtils
 
 
 object Main extends App {
-  //load in, out dir and cutoff point from the conf file
   val conf = ConfigFactory.load()
-
-  //get path of every single jpg and png from in directory
   val photos = new File(conf.getString("vars.in")).listFiles.filter(_.isFile).toList
 
-  //todo: for in FP??
-  //todo: try to remove all vars
-  //todo: tests
   photos.foreach{el => {
     val image = ImageIO.read(el)
     val s = (0 until image.getWidth())
       .map(x => (0 until image.getHeight())
-        .map(y => Calc.getMaxOfColors(image.getRGB(x, y))).sum).sum
+        .map(y => getMaxOfColors(image.getRGB(x, y))).sum).sum
     val vAVG = s/(image.getWidth*image.getHeight)
-    val score = 100 - (vAVG * 100).round
     val newName = el.getName.split("\\.")
-    if(score > conf.getInt("vars.point")){
-      newName(0) = newName(0) + "_dark_" + score
+    if(calcScore(vAVG) > conf.getInt("vars.point")){
+      newName(0) = newName(0) + "_dark_" + calcScore(vAVG)
     }else{
-      newName(0) = newName(0) + "_bright_" + score
+      newName(0) = newName(0) + "_bright_" + calcScore(vAVG)
     }
 
     val destination = conf.getString("vars.out") + separator + newName.mkString(".")
-    val src: Path = Paths.get(el.getPath)
-    val dest: Path = Paths.get(destination)
-    Files.copy(src, dest, REPLACE_EXISTING)
+    Files.copy(Paths.get(el.getPath), Paths.get(destination), REPLACE_EXISTING)
   }}
+
+  def calcScore(vAVG: Double): Int ={
+    (100 - (vAVG * 100).round).toInt
+  }
+
+  def getMaxOfColors(rgb: Int): Int ={
+
+    def getBlueFromRGB(rgb: Int): Int ={
+      rgb & 0xff
+    }
+    def getRedFromRGB(rgb: Int): Int ={
+      (rgb >> 16) & 0xff
+    }
+    def getGreenFromRGB(rgb: Int): Int ={
+      (rgb >> 8) & 0xff
+    }
+
+    (getRedFromRGB(rgb)/255 max getGreenFromRGB(rgb)/255) max getBlueFromRGB(rgb)/255
+  }
 }
